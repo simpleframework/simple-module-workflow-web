fbcb3cdb16701717756a7aa1fdecf5520b7748ca
simpleframework <cknet@126.com>
2014-01-23 14:37:06 +0800

    Signed-off-by: simpleframework <cknet@126.com>

--- src/main/java/net/simpleframework/workflow/web/page/t1/WorkflowCompleteInfoPage.java
+++ src/main/java/net/simpleframework/workflow/web/page/t1/WorkflowCompleteInfoPage.java
@@ -1,9 +1,15 @@
 package net.simpleframework.workflow.web.page.t1;
 
+import java.io.IOException;
 import java.util.Map;
 
+import net.simpleframework.ado.query.IDataQuery;
 import net.simpleframework.mvc.PageMapping;
 import net.simpleframework.mvc.PageParameter;
+import net.simpleframework.workflow.engine.ActivityBean;
+import net.simpleframework.workflow.engine.EWorkitemStatus;
+import net.simpleframework.workflow.engine.WorkitemBean;
+import net.simpleframework.workflow.web.AbstractWorkflowFormTPage;
 
 /**
  * Licensed under the Apache License, Version 2.0
@@ -15,9 +21,33 @@ import net.simpleframework.mvc.PageParameter;
 public class WorkflowCompleteInfoPage extends AbstractWorkflowFormPage {
 
 	@Override
-	public Map<String, Object> createVariables(PageParameter pp) {
-		// final WorkitemBean workitem =
-		// AbstractWorkflowFormTPage.getWorkitemBean(pp);
-		return super.createVariables(pp);
+	protected String toHtml(final PageParameter pp, final Map<String, Object> variables,
+			final String currentVariable) throws IOException {
+		final StringBuilder sb = new StringBuilder();
+		sb.append("<div class='WorkflowCompleteInfoPage'>");
+		sb.append(" <div class='l1'>#(WorkflowCompleteInfoPage.0)</div>");
+		sb.append(" <div class='l2'><table>");
+		final IDataQuery<ActivityBean> dq = aService.getNextActivities(wService
+				.getActivity(AbstractWorkflowFormTPage.getWorkitemBean(pp)));
+		ActivityBean next;
+		while ((next = dq.next()) != null) {
+			sb.append("<tr>");
+			sb.append("<td class='task'>").append(aService.getTaskNode(next)).append("</td>");
+			sb.append("<td>");
+			final IDataQuery<WorkitemBean> dq2 = wService.getWorkitemList(next,
+					EWorkitemStatus.running);
+			WorkitemBean workitem2;
+			int i = 0;
+			while ((workitem2 = dq2.next()) != null) {
+				if (i++ > 0) {
+					sb.append(", ");
+				}
+				sb.append(pp.getUser(workitem2.getUserId()));
+			}
+			sb.append("</td></tr>");
+		}
+		sb.append(" </table></div>");
+		sb.append("</div>");
+		return sb.toString();
 	}
 }
