4eb7514ef21dbe03955c48fbd8b6ae3e7a499fbb
simpleframework <cknet@126.com>
2014-12-08 13:46:00 +0800

    Signed-off-by: simpleframework <cknet@126.com>

--- src/main/java/net/simpleframework/workflow/web/WorkflowPermissionHandler.java
+++ src/main/java/net/simpleframework/workflow/web/WorkflowPermissionHandler.java
@@ -9,10 +9,13 @@ import net.simpleframework.common.ID;
 import net.simpleframework.organization.IRoleService;
 import net.simpleframework.organization.Role;
 import net.simpleframework.organization.web.OrganizationPermissionHandler;
+import net.simpleframework.workflow.engine.ActivityComplete;
 import net.simpleframework.workflow.engine.EDelegationSource;
 import net.simpleframework.workflow.engine.ProcessModelBean;
+import net.simpleframework.workflow.engine.WorkitemBean;
 import net.simpleframework.workflow.engine.participant.IWorkflowPermissionHandler;
 import net.simpleframework.workflow.engine.participant.Participant;
+import net.simpleframework.workflow.schema.UserNode;
 
 /**
  * Licensed under the Apache License, Version 2.0
@@ -25,16 +28,24 @@ public class WorkflowPermissionHandler extends OrganizationPermissionHandler imp
 
 	@Override
 	public Collection<Participant> getRelativeParticipants(final Object user, final Object role,
-			final String relative, final Map<String, Object> variables) {
+			final UserNode.RelativeRole rRole, final Map<String, Object> variables) {
 		final ArrayList<Participant> participants = new ArrayList<Participant>();
 		Role oRole = getRoleObject(role);
 		if (oRole != null) {
 			// 获取相对角色，部门
 			final IRoleService service = orgContext.getRoleService();
-			oRole = service.getRoleByName(service.getRoleChart(oRole), relative);
+			oRole = service.getRoleByName(service.getRoleChart(oRole), rRole.getRelative());
 			if (oRole != null) {
+				ID deptId = null;
+				if (rRole.isIndept()) {
+					final WorkitemBean workitem = ((ActivityComplete) variables.get("activityComplete"))
+							.getWorkitem();
+					if (workitem != null) {
+						deptId = workitem.getDeptId();
+					}
+				}
 				final ID roleId = oRole.getId();
-				final Iterator<ID> users = users(roleId, variables);
+				final Iterator<ID> users = users(roleId, deptId, variables);
 				while (users.hasNext()) {
 					participants.add(new Participant(users.next(), roleId));
 				}
